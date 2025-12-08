ccb6e6cc0970cbd35ecc87f3675fc61390eb707f
Leo Wu <r00922133@ntu.edu.tw>
2025-12-08 20:44:09 +0800

    config: rm plugin

--- apps/web/eslint.config.mjs
+++ apps/web/eslint.config.mjs
@@ -1,11 +1,9 @@
 import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
 import nextTypescript from "eslint-config-next/typescript";
-import pluginQuery from "@tanstack/eslint-plugin-query";
 
 const eslintConfig = [
   ...nextCoreWebVitals,
   ...nextTypescript,
-  ...pluginQuery.configs["flat/recommended"],
   {
     ignores: [".next", ".lintstagedrc.js", "**/*.d.ts"],
   },
