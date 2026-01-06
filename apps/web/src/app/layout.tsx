a3c031727fc43a47b5dfbde1dd95df3c3ddea5ce
Leo Wu <r00922133@ntu.edu.tw>
2026-01-06 13:50:01 +0800

    chore: clear metadata title and description in layout (#13)

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -9,8 +9,8 @@ const inter = Inter({
 });
 
 export const metadata: Metadata = {
-  title: "Baby LST",
-  description: "Baby LST",
+  title: "",
+  description: "",
 };
 
 export default function RootLayout({
@@ -20,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.variable} antialiased`}>{children}</body>
+      <body className={`${inter.variable}`}>{children}</body>
     </html>
   );
 }
